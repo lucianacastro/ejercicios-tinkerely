9b5f900c138bdb7eef63170423d4130da455848a
Luciana Castro <lucianaycastro@gmail.com>
2016-08-28 20:58:13 -0300

    agrego resolucion de ejercicio 2.2

--- problemas-secuenciales.py
+++ problemas-secuenciales.py
@@ -8,3 +8,15 @@ def get_interes():
 print get_interes()
 
 #Punto 2.2
+def get_comision():
+	sueldo_basico = float(raw_input("Ingrese el sueldo basico: "))
+	venta_1 = float(raw_input("Ingrese el monto de la primer venta: "))
+	venta_2 = float(raw_input("Ingrese el monto de la segunda venta: "))
+	venta_3 = float(raw_input("Ingrese el monto de la tercer venta: "))
+	comision = (venta_1 + venta_2 + venta_3) * 0.1
+	sueldo_total = sueldo_basico + comision
+	print "comision: ", comision
+	print "sueldo total: ", sueldo_total
+
+get_comision()
+
